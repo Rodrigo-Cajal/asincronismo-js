21d75a6aa6788f77c71ad716d102c5da6199a8ad
Rodrigo Cajal <rodrigoecajal@gmail.com>
2023-09-09 17:41:36 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const pokemonNumberInput = document.getElementById("pokemonNumber");
+const searchButton = document.getElementById("searchButton");
+const pokemonCard = document.getElementById("pokemonCard");
+const errorMessage = document.getElementById("errorMessage");
+
+searchButton.addEventListener("click", () => {
+  const pokemonNumber = pokemonNumberInput.value;
+
+  if (!pokemonNumber) {
+    errorMessage.textContent = "Please enter a valid Pokemon number.";
+    pokemonCard.innerHTML = "";
+    return;
+  }
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`)
+    .then((response) => response.json())
+    .then((data) => {
+      errorMessage.textContent = "";
+
+      const pokemonName = data.name;
+      const pokemonTypes = data.types.map((type) => type.type.name).join(", ");
+      const pokemonHeight = (data.height / 10).toFixed(1);
+      const pokemonWeight = (data.weight / 10).toFixed(1);
+      const pokemonImage = data.sprites.front_default;
+
+      const cardHTML = `
+                <div class="pokemon-info">
+                    <h2>${pokemonName}</h2>
+                    <p>Type(s): ${pokemonTypes}</p>
+                    <p>Height: ${pokemonHeight} m</p>
+                    <p>Weight: ${pokemonWeight} kg</p>
+                    <img src="${pokemonImage}" alt="${pokemonName}">
+                </div>
+            `;
+
+      pokemonCard.innerHTML = cardHTML;
+    })
+    .catch((error) => {
+      errorMessage.textContent =
+        "Pokemon not found. Please enter a valid Pokemon number.";
+      pokemonCard.innerHTML = "";
+    });
+});

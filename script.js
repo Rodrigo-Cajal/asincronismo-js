cf7e74dc68b52242fd92dc1f9ffbb0f97558ad30
Rodrigo Cajal <110480381+Rodrigo-Cajal@users.noreply.github.com>
2023-09-12 18:34:11 +0000

    modificacion-js

--- script.js
+++ script.js
@@ -3,41 +3,40 @@ const searchButton = document.getElementById("searchButton");
 const pokemonCard = document.getElementById("pokemonCard");
 const errorMessage = document.getElementById("errorMessage");
 
-searchButton.addEventListener("click", () => {
-  const pokemonNumber = pokemonNumberInput.value;
+searchButton.addEventListener("click", async () => {
+    const pokemonNumber = pokemonNumberInput.value;
 
-  if (!pokemonNumber) {
-    errorMessage.textContent = "Please enter a valid Pokemon number.";
-    pokemonCard.innerHTML = "";
-    return;
-  }
+    if (!pokemonNumber) {
+        errorMessage.textContent = "Please enter a valid Pokemon number.";
+        pokemonCard.innerHTML = "";
+        return;
+    }
 
-  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`)
-    .then((response) => response.json())
-    .then((data) => {
-      errorMessage.textContent = "";
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`);
+        const data = await response.json();
 
-      const pokemonName = data.name;
-      const pokemonTypes = data.types.map((type) => type.type.name).join(", ");
-      const pokemonHeight = (data.height / 10).toFixed(1);
-      const pokemonWeight = (data.weight / 10).toFixed(1);
-      const pokemonImage = data.sprites.front_default;
+        errorMessage.textContent = "";
 
-      const cardHTML = `
-                <div class="pokemon-info">
-                    <h2>${pokemonName}</h2>
-                    <p>Type(s): ${pokemonTypes}</p>
-                    <p>Height: ${pokemonHeight} m</p>
-                    <p>Weight: ${pokemonWeight} kg</p>
-                    <img src="${pokemonImage}" alt="${pokemonName}">
-                </div>
-            `;
+        const pokemonName = data.name;
+        const pokemonTypes = data.types.map((type) => type.type.name).join(", ");
+        const pokemonHeight = (data.height / 10).toFixed(1); // Convert to meters
+        const pokemonWeight = (data.weight / 10).toFixed(1); // Convert to kilograms
+        const pokemonImage = data.sprites.front_default;
 
-      pokemonCard.innerHTML = cardHTML;
-    })
-    .catch((error) => {
-      errorMessage.textContent =
-        "Pokemon not found. Please enter a valid Pokemon number.";
-      pokemonCard.innerHTML = "";
-    });
+        const cardHTML = `
+            <div class="pokemon-info">
+                <h2>${pokemonName}</h2>
+                <p>Type(s): ${pokemonTypes}</p>
+                <p>Height: ${pokemonHeight} m</p>
+                <p>Weight: ${pokemonWeight} kg</p>
+                <img src="${pokemonImage}" alt="${pokemonName}">
+            </div>
+        `;
+
+        pokemonCard.innerHTML = cardHTML;
+    } catch (error) {
+        errorMessage.textContent = "Pokemon not found. Please enter a valid Pokemon number.";
+        pokemonCard.innerHTML = "";
+    }
 });
